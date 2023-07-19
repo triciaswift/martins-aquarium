8c6848811a7d08471794a6fe05ed269d21d7d79d
Tricia Swift <tricia.swift8@gmail.com>
2023-07-19 16:09:02 -0500

    Created fish cards with HTML and updated CSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,20 @@
-import { getFish } from "./database.js";
+import { getFish } from "./fish/database.js";
+// Import the FishList function from the correct module
+import { FishList } from "./fish/FishList.js";
 
 const allFish = getFish();
 
 for (const fish of allFish) {
   console.log(fish);
 }
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+const parentHTMLElement = document.querySelector(".fishInserted");
+
+parentHTMLElement.innerHTML = FishList();
